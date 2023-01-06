a4615a1d0a1bd7b78ac65b9a0ceb8b136d0e307e
bevilla203 <ben.tan.villafranca@gmail.com>
2023-01-06 11:35:20 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Foundations.Assessment.BookshelfData
+
+## Overview
+
+*Flavor Text*  
+The backend team has got our API up, the library data is flowing, and a sample of the book data is waiting for you on Github. Use it to begin building the frontend of our app, Bookshelf. We will be working on Bookshelf for the next few weeks, iterating on what you write today and adding features. Be sure to establish a good foundation by following OOP best practices and documenting your code so it will be easy to pick up again later. Submit a link to your Github repo and remember to use the help desk if you get stuck.
+
+This assessment will test your ability to design classes that show a solid understanding of OOP best practices and build a small project given high level descriptions.
+
+Refer to Canvas for more information as well as the rubric.
+# Bookshelf_Project
---
+++ index.js
@@ -0,0 +1,92 @@
+// created function to randomly color each book
+function randomColorBG(){
+  let x = Math.floor(Math.random() * 256);
+  let y = Math.floor(Math.random() * 256);
+  let z = Math.floor(Math.random() * 256);
+  let bgColor = `rgb(${x}, ${y}, ${z})`;
+  return bgColor;
+}
+
+/* Book: tracks all properties in data ✅ */
+class Book {
+    constructor(author, language, subject, title){
+      this.author = author;
+      this.language = language;
+      this.subject = subject;
+      this.title = title;
+    }
+    Render(){
+      // make a rectangle to symbolize a book
+      let div = document.querySelector("#bookSection")
+      let rectangle = document.createElement("div");
+      rectangle.classList.add("bookCard");    
+      // created text element to center text in book
+      let bookTextEle = document.createElement("div"); 
+      bookTextEle.classList.add("bookText")
+      rectangle.append(bookTextEle);
+      
+      // add color to each book's background
+      rectangle.style.backgroundColor = randomColorBG();
+
+      // added innertext to book face
+      bookTextEle.innerText = 
+      `${this.title}
+      by ${this.author} 
+
+      ${this.language}
+
+      ${this.subject}
+      `
+      rectangle.inner
+      div.append(rectangle);
+    }
+}
+/*bookshelf should: 1. maintain array of shelf 2. add books to bookshelf ✅*/
+class Bookshelf {
+    constructor(){
+        this.shelf = [];
+    }
+    // adding book to bookshelf
+    Add(bookObj){
+      this.shelf.push(bookObj);
+    }
+}
+
+// create eternal bookshelf (holds all books ever created)
+let eternalBookshelf = new Bookshelf();
+
+//loop through bookData
+for (let i = 0; i < bookData.length; i++){
+  let currEle = bookData[i];
+  // create book element that will transform given object into "Book"
+  let currBook = new Book(currEle.author, currEle.language, currEle.subject, currEle.title);
+  // add book to webpage
+  currBook.Render();
+  // add book to bookshelf
+  eternalBookshelf.Add(currBook); // it worked!!!! ✅
+}
+// adding book to same shelf
+let book2 = new Book(["Clear, James"], "en", ["self-help", "non-fiction"], "Atomic Habits");
+eternalBookshelf.Add(book2); //✅
+book2.Render();
+
+// letting user add books
+let newBook = {};
+document.querySelector("#submit").addEventListener("click", function (){
+  let newTitle = document.querySelector('#Title').value;
+  let newAuthor = document.querySelector('#Author').value;
+  let newLang = document.querySelector('#Language').value;
+  let newSubj = document.querySelector('#Subject').value;
+  let newBook = new Book (newAuthor, newLang,newSubj, newTitle);
+  eternalBookshelf.Add(newBook);
+  newBook.Render();
+  document.querySelector("#newBookForm").reset();
+})
+// how do I create error if not all fields are typed in?
+
+
+
+
+
+
+
